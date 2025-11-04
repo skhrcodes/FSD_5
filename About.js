60ba0aba73e0ab22f4879254ecfc206923327e68
skhrcodes <altaccout777@gmail.com>
2025-11-04 11:12:01 +0530

    About.js

---
+++ About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function About() {
+  return (
+    <div className="container">
+      <h3>About This App</h3>
+      <p>This is a simple Task Manager built with React Router. It demonstrates navigation between pages.</p>
+    </div>
+  );
+}
+
+export default About;
